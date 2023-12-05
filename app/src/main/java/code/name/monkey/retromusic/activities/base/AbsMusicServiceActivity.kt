c0fea40eba8c1b0f971c00d3247df4da09e95062
prpr894 <wangjunpu2012@outlook.com>
2023-12-05 10:03:42 +0800

    更新打包配置，去除通知权限。

--- app/src/main/java/code/name/monkey/retromusic/activities/base/AbsMusicServiceActivity.kt
+++ app/src/main/java/code/name/monkey/retromusic/activities/base/AbsMusicServiceActivity.kt
@@ -184,7 +184,7 @@ abstract class AbsMusicServiceActivity : AbsBaseActivity(), IMusicServiceEventLi
         return mutableListOf<String>().apply {
             if (VersionUtils.hasT()) {
                 add(Manifest.permission.READ_MEDIA_AUDIO)
-                add(Manifest.permission.POST_NOTIFICATIONS)
+//                add(Manifest.permission.POST_NOTIFICATIONS)
             } else {
                 add(Manifest.permission.READ_EXTERNAL_STORAGE)
             }
